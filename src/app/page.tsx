4fbaea949598eba66b2a14462604bc97e3a4e235
MrGhostsnow <96500538+MrGhostsnow@users.noreply.github.com>
2023-10-17 11:37:03 -0300

    fix: navbar rendering failure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
 import HomePage from "./components/Home";
 import NavBar from "./components/NavBar";
-import { ContainerMain } from "./styles";
+import { ContainerMain, ContainerNavBar, Title } from "./styles";
+import styles from "./page.module.css";
+
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <ContainerMain>
-      <NavBar />
+    <div className={styles.containerMain}>
+      <div className={styles.containerNavBar}>
+        <Link href="/">
+          <p className={styles.title}>Movies</p>
+        </Link>
+      </div>
+      {/* <NavBar /> */}
       <HomePage />
-    </ContainerMain>
+    </div>
   );
 }
--- src/app/styles.js
+++ src/app/styles.js
@@ -9,3 +9,20 @@ export const ContainerMain = styled.div`
   height: 100%;
   background-color: white;
 `;
+
+export const ContainerNavBar = styled.div`
+  display: flex;
+  width: 100%;
+  height: 90px;
+  align-items: center;
+  justify-content: center;
+  background-color: #1f75cb;
+  @media screen and (max-width: 480px) {
+    margin-bottom: 12rem;
+  }
+`;
+
+export const Title = styled.h1`
+  color: #2fe4ed;
+  cursor: pointer;
+`;
