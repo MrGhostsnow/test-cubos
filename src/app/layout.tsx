3d6f3bdc8e127db219902ce1c63c714291993f75
MrGhostsnow <96500538+MrGhostsnow@users.noreply.github.com>
2023-11-27 17:26:57 -0300

    fix: font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter, Abel, Lato } from "next/font/google";
+// import { Inter, Abel, Lato } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
-const abel = Abel({ subsets: ["latin"], weight: "400" });
-const lato = Lato({ subsets: ["latin"], weight: "400" });
+// const inter = Inter({ subsets: ["latin"] });
+// const abel = Abel({ subsets: ["latin"], weight: "400" });
+// const lato = Lato({ subsets: ["latin"], weight: "400" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-      <body className={abel.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
